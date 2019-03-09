dbc21b9a9940685d761a5a2838e5fce18f2981bf
shukebeta <weizhong2004@gmail.com>
2019-03-10 10:42:21 +1300

    Mission Accomplished!

--- mean_and_sd/src/index.js
+++ mean_and_sd/src/index.js
@@ -1,34 +1,47 @@
 let app = new Vue({
   el: '#app',
   data: {
-
+    message: 'hello, vue',
+    filedata: [
+    ],
+    result:[]
   },
   methods: {
     fileChangeHandler: function (event) {
-      "use strict"  
-      // console.log("file has changed!")   
-      // console.log("here 1")
-
+      let self = this
+      "use strict" 
       Object.keys(event.target.files).forEach(i => {
         const file = event.target.files[i];
         const reader = new FileReader();
         reader.onload = (event) => {
-          //server call for uploading or reading the files one-by-one
-          //by using 'reader.result' or 'file'
-                  // console.log("file has loaded")
-          // console.log(event.target.result);
-          let user = event.target.result.split('\r\n');
-          console.log(user);
-          // console.log("here 3");
-          
-          localStorage.setItem('user', JSON.stringify({username: user[0], password: user[1]}));
-          let result = JSON.parse(localStorage.getItem('user'));
-          console.log(JSON.stringify(user));
-          console.log(result);
+          let data = event.target.result.trim().split(/[\r\n \t]+/);
+          self.filedata.push({fileName: file.name, dataList: data})
+          console.log(self.filedata)
         }
         reader.readAsText(file);
       })
-      // console.log("here 2");
+    },
+    calculateMean: dataList => {
+      let total = 0;
+      for (let num of dataList) {
+        total += +num
+      }
+      return dataList.length ? (total / dataList.length).toFixed(2) : 0
+    },
+    calculateStandardDeviation: function (dataList, mean) {
+      let sum = 0;
+      for (let num of dataList) {
+        sum += (num - mean) * (num - mean)
+      }
+      return dataList.length > 1 ? Math.sqrt (sum / (dataList.length - 1)).toFixed(8) : null
+    },
+    calculate: function() {
+      for (let tableData of this.filedata) {
+        tableData.mean = this.calculateMean (tableData.dataList)
+        tableData.standardDeviation = this.calculateStandardDeviation(tableData.dataList, tableData.mean)
+      }
+      this.result = []
+      return this.result = this.filedata
     }
   }
 });
