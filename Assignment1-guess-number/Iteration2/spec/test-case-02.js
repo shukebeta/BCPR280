8667715632830f1c94193e21a2de2ca9ff4c271e
shukebeta <weizhong2004@gmail.com>
2019-03-31 20:11:34 +1300

    add test case to iteration 2

---
+++ Assignment1-guess-number/Iteration2/spec/test-case-02.js
@@ -0,0 +1,36 @@
+describe("make a guess", function () {
+  'use strict';
+  describe("generate a secret number", function () {
+      var theGuessObject;
+      beforeEach(function () {
+          theGuessObject = new Guess(42);
+      });
+
+      it("the secretNumber should be 42", function () {
+          expect(theGuessObject.secretNumber == 42).toBe(true);
+      });
+
+      it("make a guess 41 should be return HOT", function () {
+          expect(theGuessObject.makeGuess(41)).toBe('HOT')
+      });
+
+      it("make a guess 56 should be return WARM", function () {
+        expect(theGuessObject.makeGuess(56)).toBe('WARM')
+      });
+
+      it("make a guess 76 should be return COOL", function () {
+        expect(theGuessObject.makeGuess(76)).toBe('COOL')
+      });
+
+      it("make a guess 99 should be return COLD", function () {
+        expect(theGuessObject.makeGuess(99)).toBe('COLD')
+      });
+      
+      it("make a guess 42 should be return 'You got it in 3 trials!'", function () {
+        theGuessObject.makeGuess(1);
+        theGuessObject.makeGuess(2);
+        expect(theGuessObject.makeGuess(42)).toBe('You got it in 3 trials!')
+      });
+
+    });
+});
