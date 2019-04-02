ff5292ac3d04b73fab697dcc141b1a558720cd57
shukebeta <weizhong2004@gmail.com>
2019-04-02 13:41:15 +1300

    standard --fix

--- Assignment1-guess-number/Iteration1/src/controller.js
+++ Assignment1-guess-number/Iteration1/src/controller.js
@@ -11,39 +11,38 @@ let app = new Vue({
     currentGame: null
   },
   methods: {
-    startGame: function (event) {      
+    startGame: function (event) {
       setTimeout(() => {
-        this.currentGame = new Guess();
-        this.secretNumber = this.currentGame.secretNumber;
+        this.currentGame = new Guess()
+        this.secretNumber = this.currentGame.secretNumber
         this.startMessage = 'Restart'
         this.promptMessage = ''
         this.guessHistory = []
         this.tryCount = 0
         this.$nextTick(() => this.$refs.input.focus())
       }, 500)
-      
     },
-    submitYourInput: function() {
+    submitYourInput: function () {
       this.yourCurrentGuess = +this.yourCurrentGuess
       if (Number.isInteger(this.yourCurrentGuess)) {
         if (!this.currentGame.isValidNumber(this.yourCurrentGuess)) {
-          alert("You must enter a number between 0 and 99!")
+          alert('You must enter a number between 0 and 99!')
           this.yourCurrentGuess = ''
           return
         }
       } else {
-        alert("You must enter a number!")
+        alert('You must enter a number!')
         this.yourCurrentGuess = ''
         return
       }
 
-      this.promptMessage = this.currentGame.makeGuess(this.yourCurrentGuess);
-      if (this.promptMessage.startsWith("You got it")) {
+      this.promptMessage = this.currentGame.makeGuess(this.yourCurrentGuess)
+      if (this.promptMessage.startsWith('You got it')) {
         this.startMessage = 'Start'
       }
 
-      this.guessHistory.push({tryCount: this.currentGame.tryCount, number: this.yourCurrentGuess})
+      this.guessHistory.push({ tryCount: this.currentGame.tryCount, number: this.yourCurrentGuess })
       this.yourCurrentGuess = ''
     }
   }
-});
+})
