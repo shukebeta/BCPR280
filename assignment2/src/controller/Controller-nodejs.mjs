54ab308ee14e82bad3f145336f3da077ff5c7e95
shukebeta <weizhong2004@gmail.com>
2019-05-14 09:47:11 +1200

    make Controller-nodejs.mjs executeable, and try to optimise the cli interface

--- assignment2/src/controller/Controller-nodejs.mjs
+++ assignment2/src/controller/Controller-nodejs.mjs
@@ -1,3 +1,4 @@
+#!/usr/bin/env node --experimental-modules
 
 import {ReadFileAndCalculate} from "../bin/read-data-file-and-calculate.mjs"
 import fs from 'fs'
@@ -8,20 +9,27 @@ import chalk from 'chalk'
 const dataFileFolder = '../test-data/'
 let fileList
 
+console.clear()
+
+let getColorText = (error, color) => {
+  color = color || 'red'
+  return chalk[color](error)
+}
+
+let colorLog = (error, color) => {
+  console.log(getColorText(error, color))
+}
+
+
 let getPrompt = () => {
-  let promptStrList = [chalk.green("data file list:")]
+  let promptStrList = [chalk.greenBright("data file list:")]
   fileList = []
   fs.readdirSync(dataFileFolder).map((file, index) => {
     fileList.push(file)
-    promptStrList.push((index + 1) + ' ' + file)
+    promptStrList.push(getColorText((index + 1).toString(), 'yellow') + ' ' + file)
   })
   promptStrList.push(chalk.greenBright('Please input two space separated number ahead the data file> '))
   return promptStrList.join("\n")
-}
-
-let errorLog = (error) => {
-  const eLog = chalk.red(error)
-  console.log(eLog)
 }
 
 const rl = readline.createInterface({
@@ -33,22 +41,22 @@ const rl = readline.createInterface({
 rl.prompt();
 
 rl.on('line', (line) => {
+  console.clear()
   let fileIndexList = line.trim().split(/[, ]+/).map(num => num - 1)
 
   if (fileIndexList.length != 2) {
-    errorLog('invalid input: you can and only can input two number')
+    colorLog('invalid input: you can and only can input two number')
     rl.prompt()
     return
   }
 
   for (let fileIndex of fileIndexList) {
     if (fileIndex + 1 > fileList.length) {
-      errorLog('invalid input: your input is too big!')
+      colorLog('invalid input: your input is too big!')
       rl.prompt()
       return
     }
   }
-
   (new ReadFileAndCalculate(
     dataFileFolder + fileList[fileIndexList[0]],
     dataFileFolder + fileList[fileIndexList[1]])).calculateAndOutput(() => rl.prompt())
